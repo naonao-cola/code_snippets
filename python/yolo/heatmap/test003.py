6a8e30bfe5a18912713dc79266a4b79f05fdbf5c
weiwei.wang <1319144981@qq.com>
2025-10-20 17:31:59 +0800

    fix: add files

---
+++ python/yolo/heatmap/test003.py
@@ -0,0 +1,18 @@
+﻿from ultralytics import YOLO
+from pathlib import Path
+
+weights = "/home/greatek/huangsc/test001/run/konghu2/weights/best.pt"
+img_path = "/home/greatek/wangww/demo/py/test003/66e05ccc3f89422c9a0540097f4462ca.png"
+out_dir = Path("/home/greatek/wangww/demo/py/test003/output/my_feats")          # 你想放的目录
+
+model = YOLO(weights)
+
+# 只改 project/name，让 YOLO 自动创建 output/my_feats/feature_maps/
+model.predict(
+    img_path,
+    visualize=True,        # 打开特征图可视化
+    project=out_dir.parent,  # 对应 output
+    name=out_dir.name        # 对应 my_feats
+)
+
+print("特征图已保存到:", out_dir / "feature_maps")
