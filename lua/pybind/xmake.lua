ca1f3f354d26d0748596597425e31a33ee33da28
weiwei.wang <1319144981@qq.com>
2025-11-07 13:19:27 +0800

    fix: add files

--- lua/pybind/xmake.lua
+++ lua/pybind/xmake.lua
@@ -7,18 +7,16 @@ add_requires("opencv 4.8.x",{system = false})
 
 
 -- 添加依赖
-add_requires("python 3.10")
+--add_requires("python 3.10")
 add_requires("pybind11")
-add_requireconfs("pybind11.python",{version = "3.10",override=true})
+--add_requireconfs("pybind11.python",{version = "3.10",override=true})
 
 target("example")
-    set_kind("shared")
-    set_extension(".pyd")
-    add_rules("python.library")
+    add_rules("python.module")
     -- 添加依赖
     add_packages("pybind11")
     add_packages("opencv")
     -- 设置编译路径
-    set_targetdir("$(projectdir)/libs")
+    --set_targetdir("$(projectdir)/libs")
     -- 添加文件
     add_files("$(projectdir)/src/example.cpp")
